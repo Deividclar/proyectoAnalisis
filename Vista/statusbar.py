5f987c37c47a9d02e3e2c8ae872d2a98fc4564aa
DeividClar <deividclar@gmail.com>
2019-04-19 02:07:24 -0500

    editor v1

---
+++ Vista/statusbar.py
@@ -0,0 +1,26 @@
+# encoding: utf-8
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+
+# -------------------------------------------------------------------------------------------------
+#   clase StatusBar : -> hereda de QStatusBar
+#           contiene la posicion del cursor en el editor
+# -------------------------------------------------------------------------------------------------
+class StatusBar(QStatusBar):
+    # -------------------------------------------------------------------------------------------------
+    #       constructor del status bar, inicialmente en la posicion 0,0
+    # -------------------------------------------------------------------------------------------------
+    def __init__(self):
+        super(StatusBar, self).__init__()
+
+        self.posicion_cursor = "Linea: %s, Columna: %s"
+        self.linea_columna=QLabel(self.posicion_cursor % (0,0))
+        self.addWidget(self.linea_columna)
+
+    # -------------------------------------------------------------------------------------------------
+    #       metodo para actualizar el label de la posicion del cursor
+    # -------------------------------------------------------------------------------------------------
+    def actualizar_label(self,linea,columna):
+        self.linea_columna.setText(self.posicion_cursor % (linea,columna))
